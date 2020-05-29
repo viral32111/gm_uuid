7bc03da369849ed11de906ae3946ffa4a9ef53d5
viral32111 <19510403+viral32111@users.noreply.github.com>
2020-05-30 00:10:45 +0100

    Updated premake script
    * Added plenty of comments to explain everything.
    * Removed usage of deprecated functions.
    * Cleaned up the formatting/code style.
    * Dropped support for x86 in favour of x64 being the future of Garry's Mod.
    * Dropped support for macOS (I'm unable to test this platform).

--- scripts/premake5.lua
+++ scripts/premake5.lua
@@ -1,24 +1,56 @@
-workspace "gm_uuid"
-	configurations {"Release"}
-	location ("project")
+-- Store operating system string
+local os = os.target()
 
-project "gm_uuid"
-	kind "SharedLib"
-	architecture "x86"
-	language "C++"
-	includedirs "include"
-	targetdir "build"
-	symbols "On"
+-- Define the solution & project
+workspace ( "UUID" )
+	-- Set the file name
+	filename ( "uuid" )
 
-	if (os.is("windows")) then targetsuffix "_win32" end
-	if (os.is("macosx")) then targetsuffix "_osx" end
-	if (os.is("linux")) then targetsuffix "_linux" end
+	-- Generated project files directory
+	location ( "../project/" .. os .. "/" )
 
-	configuration "Release"
-		optimize "Speed"
-		flags "StaticRuntime"
-
-	files {
-		"source/**.*",
-		"include/**.*",
+	-- Configurations
+	configurations {
+		"Release"
 	}
+
+	project ( "UUID" )
+		-- Set the file name
+		filename ( "uuid" )
+
+		-- Misc C++ stuff
+		language ( "C++" )
+		kind ( "SharedLib" )
+		staticruntime ( "On" )
+		symbols ( "On" )
+		optimize ( "Speed" )
+		architecture ( "x86_64" )
+
+		-- Generated project files directory
+		location ( "../project/" .. os .. "/" )
+
+		-- Build/output directory
+		objdir ( "../build/" .. os .. "/obj/" )
+		targetdir ( "../build/" .. os .. "/" )
+
+		-- Output name & extension
+		targetname ( "gm_uuid" )
+		targetextension ( ".dll" )
+
+		-- Additional include directories
+		includedirs {
+			"../include/"
+		}
+
+		-- Source files
+		files {
+			"../source/**.*"
+		}
+		
+		-- Per-OS module suffix & extension
+		if ( os == "windows" ) then
+			targetsuffix ( "_win64" )
+		elseif ( os == "linux" ) then
+			targetprefix ( "" ) -- Linux defaults to lib prefix
+			targetsuffix ( "_linux64" )
+		end
