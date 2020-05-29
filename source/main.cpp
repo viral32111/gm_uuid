858033b8d78d7cbfec5efb3dc03f33a5eb299242
viral32111 <19510403+viral32111@users.noreply.github.com>
2020-05-30 00:13:26 +0100

    Updated source code
    * Commented everything to make it easier for others.
    * Removed namespace imports.
    * Cleaned up formatting/code style.
    * Moved argument checking out of try-catch statement.
    * Removed the uuid table and instead just made it into a global function to follow the rest of the Garry's Mod style.

--- source/main.cpp
+++ source/main.cpp
@@ -1,50 +1,71 @@
+// Standard headers
 #include <algorithm>
 #include <string>
-using namespace std;
 
+// Boost headers
+#include <boost/lexical_cast.hpp>
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_generators.hpp>
 #include <boost/uuid/uuid_io.hpp>
-using namespace boost::uuids;
 
-#include <boost/lexical_cast.hpp>
-using namespace boost;
+// Garry's Mod headers
+#include <GarrysMod/Lua/Interface.h>
 
-#include "GarrysMod/Lua/Interface.h"
-using namespace GarrysMod::Lua;
+// Global random UUID generator
+boost::uuids::random_generator randomUUIDGenerator;
 
-random_generator randomUUIDGenerator;
+// Generate function
+LUA_FUNCTION( GenerateUUID ) {
+	// Include dashes by default
+	bool includeDashes = true;
+	
+	// Check for the include dashes argument
+	if ( LUA->IsType( 1, GarrysMod::Lua::Type::BOOL ) == true ) {
+		// Set include dashes to what the user provides
+		includeDashes = LUA->GetBool( 1 );
+	}
 
-LUA_FUNCTION(generate){
+	// Try-catch statement for catching any exceptions from Boost
 	try {
-		bool includeDashes = true;
-		if (LUA->IsType(1, Type::BOOL))includeDashes = LUA->GetBool(1);
+		// Create the UUID string
+		std::string uuid = boost::lexical_cast<std::string>( randomUUIDGenerator() );
 
-		string uuid = lexical_cast<string>(randomUUIDGenerator());
-		if (includeDashes == false)uuid.erase(std::remove(uuid.begin(), uuid.end(),'-'),uuid.end());
+		// Remove dashes if specified
+		if ( includeDashes == false ) {
+			uuid.erase( std::remove( uuid.begin(), uuid.end(), '-' ), uuid.end() );
+		}
 
-		LUA->PushString(uuid.c_str());
-	} catch (const std::exception & ex) {
-		LUA->ThrowError(ex.what());
+		// Return the UUID as a char string
+		LUA->PushString( uuid.c_str() );
+	} catch ( const std::exception & ex ) {
+		// Throw a Lua error containing the error message
+		LUA->ThrowError( ex.what() );
+
+		// Return nil
 		LUA->PushNil();
 	}
 
+	// Return a single argument
 	return 1;
 }
 
-GMOD_MODULE_OPEN(){
-	randomUUIDGenerator=random_generator();
+// Runs when the module is required for the first time
+GMOD_MODULE_OPEN() {
+	// Set the random UUID generator global
+	randomUUIDGenerator = boost::uuids::random_generator();
 
-	LUA->PushSpecial(SPECIAL_GLOB);
-		LUA->CreateTable();
-			LUA->PushCFunction(generate);
-			LUA->SetField(-2,"generate");
-		LUA->SetField(-2,"uuid");
+	// Push the generate function to the global table
+	LUA->PushSpecial( GarrysMod::Lua::SPECIAL_GLOB );
+		LUA->PushCFunction( GenerateUUID );
+		LUA->SetField( -2, "GenerateUUID" );
 	LUA->Pop();
 
+	// Return a single argument
 	return 1;
 }
 
-GMOD_MODULE_CLOSE(){
+// Runs on environment shutdown
+GMOD_MODULE_CLOSE() {
+	// Return zero arguments
 	return 0;
 }
